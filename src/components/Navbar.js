e9e5e75f84cdcfc7d7949045050484fff4a659d7
Gnani-is-Coding <gnanendragariminti@nexocide.com>
2024-04-07 10:14:24 +0530

    adds Become ACCA section

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,14 @@
-import Button from 'react-bootstrap/Button';
+
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { CgProfile } from "react-icons/cg";
 
 function NavScrollExample() {
   return (
     <Navbar expand="lg" style={{ backgroundColor: '#33107a', color: '#ffffff' }}>
       <Container fluid>
-        <Navbar.Brand href="#" style={{ color: '#ffffff' }}>Navbar-scroll.com</Navbar.Brand>
+        <Navbar.Brand href="#" style={{ color: '#ffffff' }}><span className="badge text-bg-secondary fs-6">ReactBootstrap.com </span></Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
@@ -17,32 +16,20 @@ function NavScrollExample() {
             style={{ maxHeight: '100px', color: '#ffffff' }}
             navbarScroll
           >
-            <Nav.Link href="#action1" style={{ color: '#ffffff' }}>Home</Nav.Link>
-            <Nav.Link href="#action2" style={{ color: '#ffffff' }}>About</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown" style={{color: '#ffffff' }}>
-              <NavDropdown.Item href="#action3" >Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+            <Nav.Link href="#action1" style={{ color: '#ffffff' }} className='text-white fw-semibold'>Home</Nav.Link>
+            <Nav.Link href="#action2" style={{ color: '#ffffff' }} className='text-white fw-semibold'>ACC</Nav.Link>
+            <Nav.Link href="#action2" style={{ color: '#ffffff' }} className='text-white fw-semibold'>Placements</Nav.Link>
+            <Nav.Link href="#action2" style={{ color: '#ffffff' }} className='text-white fw-semibold'>Alumni</Nav.Link>
+            <Nav.Link href="#action2" style={{ color: '#ffffff' }} className='text-white fw-semibold'>About Us</Nav.Link>
+            
+           
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-              style={{ color: '#ffffff' }}
-            />
-            <Button variant="outline-primary" style={{ color: '#ffffff' }}>Search</Button>
-          </Form>
+          <div className="d-flex align-center border rounded p-1 bg-white text-black" variant="outline-primary">
+            <div className='me-2'>
+            <CgProfile/>
+            </div>
+            <div className=' fw-bold'>Login/SignUp</div>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
